597da413e266a2ecd9b2fb899a40694c2ecc9b62
Thalita Portes <thalitafernandaportes55@gmail.com>
2022-12-10 15:06:56 -0300

    Subindo o projeto pela primeira vez.

---
+++ app/src/main/java/com/projetokotlin/conversordetemperatura/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.projetokotlin.conversordetemperatura
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.FirebaseApp
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
